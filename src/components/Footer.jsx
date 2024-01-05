3f332d314a58aa1c0c33475c468781c348876c41
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2024-01-05 15:12:33 +0530

    adding hero section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-black p-4 flex justify-center items-center">
-       <h1 className="text-white">	&copy; Joel Henry</h1>
+    <div className="bg-black p-4 flex flex-col justify-center items-center text-white">
+       <h1 className="flex items-center gap-2">Made with <span><FaHeart color="red" /></span> by Joel </h1>
+       <h1>	&copy; Joel Henry</h1>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
 import Images from "../assets/Images/Images"
 import Typewriter from "typewriter-effect";
+import HeroBgAnimation from "./HeroBgAnimation";
 
 const Hero = () => {
     const roles= [ "Full Stack Developer", "Android Developer", "UI/UX Designer", "Programmer" ]
-    const textXl = 'text-6xl font-semibold'
+    const textXl = 'text-5xl md:text-6xl font-semibold my-4'
   return (
-    <div className="flex items-center justify-center flex-wrap p-4 text-white">
-      <div className="basis-1/2">
-        <div className="my-4">
-            <h1 className={textXl}>Hi, I am</h1>
-            <h1 className={textXl}>Joel Henry</h1>
-        </div>
-        <h1 className="flex gap-2 my-4 text-4xl font-semibold">
+    <div className="flex items-center justify-center flex-wrap-reverse p-4 text-white gap-16 md:gap-0">
+      <div className="md:basis-1/2 md:pl-52 text-center md:text-left">
+        <h1 className={textXl}>Hi, I am</h1>
+        <h1 className={textXl}>Joel Henry</h1>
+        <h1 className="flex justify-center md:justify-normal gap-2 my-4 text-xl md:text-4xl font-semibold">
          I am a
-         <span className="text-purplish">
+         <span className="text-purplish cursor-pointer">
             <Typewriter
                 options={{
                     strings: roles,
@@ -24,11 +23,12 @@ const Hero = () => {
              />
          </span>
         </h1>
-        <p className="w-2/3">I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.</p>
-        <button style={{background: 'linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%)'}}><Link to='https://drive.google.com/file/d/1C96IINgbB6MYDdTgaSQHY2IWicBt_UIR/view?usp=sharing' target='_blank'>Resume</Link></button>
+        <p className="w-full mb-4 text-xl opacity-50">I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.</p>
+        <button className="resumebtn px-16 py-4 text-xl font-semibold rounded-2xl shadow-xl hover:scale-110" style={{background: 'linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%)'}}><Link to='https://drive.google.com/file/d/1C96IINgbB6MYDdTgaSQHY2IWicBt_UIR/view?usp=sharing' target='_blank'>Check Resume</Link></button>
       </div>
-      <div className="basis-1/4">
-        <img className="w-full" src={Images.HeroImage} alt="Hero Image" />
+      <div className="basis-full md:basis-1/2 relative">
+        <HeroBgAnimation />
+        <img className="absolute top-1/4 left-16 rounded-xl w-2/3 md:top-32 md:left-32 md:w-2/3" src={Images.HeroImage} alt="Hero Image" />
       </div>
     </div>
   )
